cd85d149ccf25b756b60a74ce7d3b721f596d9cc
winterroad <ullakaija.talvitie@gmail.com>
2018-04-11 16:31:51 +0300

    Feature: requirements changed: 2 cats + catname visible
    - modified the countUp for 2 cats with same Counters
    - modified css to take in account 2 cats

--- catclicker.js
+++ catclicker.js
@@ -1,10 +1,28 @@
-let counter = 0;
-const cat = document.getElementById("cat");
-const countDisplay = document.getElementById("clicks");
+let counter;
+const cats = document.getElementById("catBox");
+const cat1 = document.getElementById("catname1");
+const cat2 = document.getElementById("catname2");
+const countDisplay1 = document.getElementById("clicks1");
+const countDisplay2 = document.getElementById("clicks2");
 
-cat.addEventListener('click', countUp);
 
-function countUp(){
-  counter++;
-  countDisplay.textContent = counter;
+nameTheCats("Jack", "Bauer");
+
+cats.addEventListener('click', countUp);
+
+function nameTheCats(catname1, catname2){
+    cat1.textContent = catname1;
+    cat2.textContent = catname2;
+}
+
+function countUp(e){
+    if(e.target.id == "cat1") {
+      counter = Number(countDisplay1.textContent);
+      counter++;
+      countDisplay1.textContent = counter;
+    } else if(e.target.id == "cat2"){
+        counter = Number(countDisplay2.textContent);
+        counter++;
+        countDisplay2.textContent = counter;
+      }
 }
